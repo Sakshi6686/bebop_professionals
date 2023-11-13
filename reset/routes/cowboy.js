aaa4e0e203f531ee949deff5428929063ca53497
shreyarakesh <127003176+shreyarakesh@users.noreply.github.com>
2023-11-13 22:48:21 +0530

    Add files via upload

---
+++ reset/routes/cowboy.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const router = express.Router()
+const cowboym = require('../models/cowboym')
+net:
+  ssl:
+    mode: requireSSL
+    PEMKeyFile: /path/to/mongodb.pem
+
+router.get('/signup', (req, res) => {
+    res.render('signup')
+})
+router.get('/', (req, res) => {
+    res.render('login')
+})
+router.get('/forgotPassword', (req, res) => {
+    res.render('forgotPassword')
+})
+router.get('/resetPassword', (req, res) => {
+    res.render('resetPassword')
+})
+
+router.get('/', async(req,res) =>{
+    console.log('#####  Get Request ##### ')
+    try{
+const cowboym = require('../models/cowboym')
+        const cowboys = await cowboym.find()
+        res.json(cowboys)
+
+    }catch(err){
+        res.send('Error ' + err)
+    }
+})
+
+router.post('/', async(req,res) => {
+     const cowboys = new cowboym({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        hint: req.body.hint
+    })
+    try{
+        const cb1 = await cowboys.save()
+        res.json(cb1)
+
+    }catch(err){
+        res.send("Error " + err)
+    }
+
+
+})
+
+module.exports = router

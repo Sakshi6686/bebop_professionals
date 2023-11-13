59dcbcb97869cb23c89b862a2b57bcd81aebe0ad
Sakshi <sakshi6686.g@gmail.com>
2023-11-13 20:33:32 +0530

    Created registration form in react

---
+++ client/bebop/src/App.js
@@ -0,0 +1,12 @@
+ 
+import React from 'react';
+import RegistrationForm from './registration/registration.js';
+function App() {
+  return (
+     <>
+<RegistrationForm/>
+     </>
+  );
+}
+
+export default App;

9655bed5f740d7fb45882f07c7eb7a2b60fbb58e
Sakshi <sakshi6686.g@gmail.com>
2023-11-14 23:47:00 +0530

    Modified client folder

--- client/registration/reg.js
+++ client/registration/reg.js
@@ -17,26 +17,17 @@ document.addEventListener('DOMContentLoaded', function () {
     form.addEventListener('submit', function (e) {
         e.preventDefault();
 
-        // Your form submission logic goes here
-
-        // Access form data using document.getElementById or other DOM methods
-        // For example:
-        // const firstName = document.getElementsByName('firstName')[0].value;
-        // const lastName = document.getElementsByName('lastName')[0].value;
-        // const email = document.getElementsByName('email')[0].value;
-        // const password = document.getElementsByName('password')[0].value;
-        // const confirmPassword = document.getElementsByName('confirmPassword')[0].value;
-        // const country = document.getElementsByName('country')[0].value;
-        // const city = document.getElementsByName('city')[0].value;
-        // const userType = document.getElementById('userType').value;
-        // const collegeName = document.getElementsByName('collegeName')[0].value;
-        // const startingYear = document.getElementsByName('startingYear')[0].value;
-        // const endingYear = document.getElementsByName('endingYear')[0].value;
-        // const companyName = document.getElementsByName('companyName')[0].value;
-        // const position = document.getElementsByName('position')[0].value;
-        // const profStartingYear = document.getElementsByName('profStartingYear')[0].value;
-        // const profEndingYear = document.getElementsByName('profEndingYear')[0].value;
-
-        // Perform any necessary validation and submission
+       const password=document.getElementById("pass");
+       
+       const confirmPassword=document.getElementById("confirmPass");
+       const passwordError = document.getElementById('passwordError');
+       if (password !== confirmPassword) {
+        passwordError.textContent = 'Passwords do not match';
+        return false;
+    } else {
+        passwordError.textContent = '';
+        return true;
+    }
+       
     });
 });
